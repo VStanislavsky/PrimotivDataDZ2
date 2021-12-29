d62333a68f0e8c5997d7363828299fce63546bb1
VStanislavsky <stanislavvlasov@mail.ru>
2021-12-29 18:37:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 100;
+        int refill = 1001;
+        System.out.println(balance+refill);
+        int bonuses = 100;
+        if (refill >= 1001)
+            System.out.println(refill/bonuses);
+
+
+    }
+}
